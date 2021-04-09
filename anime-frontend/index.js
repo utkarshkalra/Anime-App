fe5c14fafd985c395a1fc5ec614e8177ce42d674
utkarshkalra <utkarshkalra.2001@gmail.com>
2021-04-09 23:19:54 +0530

    hidden update page

--- anime-frontend/index.js
+++ anime-frontend/index.js
@@ -6,39 +6,13 @@ const element = document.getElementById("up");
 
 //adding scroll;
 
-window.smoothScroll = function (target) {
-  var scrollContainer = target;
-  do { //find scroll container
-    scrollContainer = scrollContainer.parentNode;
-    if (!scrollContainer) return;
-    scrollContainer.scrollTop += 1;
-  } while (scrollContainer.scrollTop == 0);
-
-  var targetY = 0;
-  do { //find the top of target relatively to the container
-    if (target == scrollContainer) break;
-    targetY += target.offsetTop;
-  } while (target = target.offsetParent);
-
-  scroll = function (c, a, b, i) {
-    i++; if (i > 30) return;
-    c.scrollTop = a + (b - a) / 30 * i;
-    setTimeout(function () { scroll(c, a, b, i); }, 20);
-  }
-  // start scrolling
-  scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
-}
-
 getData();
 async function getData() {
   const res = await fetch(URL);
   const data = await res.json();
   console.log(data);
 
-
-
   data.forEach((item) => {
-
 
     const card = document.createElement("div");
     const image = document.createElement("img");
@@ -120,13 +94,16 @@ wrap.addEventListener("click", (e) => {
 
     let name = parent.querySelector(".card-title").textContent;
     let des = parent.querySelector(".card-text").textContent;
+
+
     if (confirm(`do u wanna update ${name} card `)) {
       var updatename = document.getElementById("name");
       var updatedes = document.getElementById("description");
       var button = document.getElementById("btn");
       updatename.value = name;
       updatedes.value = des;
-      smoothScroll(element);
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
       button.addEventListener("click", (e) => {
         fetch(`${URL}/${id}`, {
           method: "PATCH",
@@ -141,8 +118,9 @@ wrap.addEventListener("click", (e) => {
         })
           .then(res => res.json())
           .then(() => {
-            alert("updated!")
+
             location.reload()
+
           })
       })
     }
